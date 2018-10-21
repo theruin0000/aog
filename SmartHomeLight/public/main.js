d7187d03832c1ec19e65bbdb20a673fcb4f36f27
theruin0000 <theruin0000@naver.com>
2018-10-21 20:54:24 +0900

    complete for fcm example

--- SmartHomeLight/public/main.js
+++ SmartHomeLight/public/main.js
@@ -19,12 +19,44 @@
 function Main() {
   document.addEventListener('DOMContentLoaded', function () {
     // Bind events.
-    this.updateButton = document.getElementById('smart-home-light-update');
-    this.updateButton.addEventListener('click', this.updateState.bind(this));
+    // this.updateButton = document.getElementById('smart-home-light-update');
+    // this.updateButton.addEventListener('click', this.updateState.bind(this));
+
+    let elOnOff = document.getElementById('light-onOff');
+    elOnOff.addEventListener("change", this.updateState.bind(this));
+
     this.light = document.getElementById('light');
     this.initFirebase();
     this.initLighter();
   }.bind(this));
+}
+
+function sendMessage(isOn) {
+  const key = 'AAAAhmYf7_Y:APA91bFNrhAdK-yNpZb7r4-fHLD7cqidZVLWT0nvS77XYFNTls6sott0JZ5o8aEvzS6DrWnPNHz_KajrF_r3fijyXfSYmk7CMzM_R8NXLAn_ooxKIdg9_yFLN6UHXLnPvwVE9Qe1oWZE';
+  const to = 'cIJp5mws_Ok:APA91bHvJfJJU73zckE6qJVHCoxGQNA7TLkyIdTQfq4Iu7D7EPUTvbKNdvloQUjee9rOf4umNbyWeo-xE-a849vwAahkx-byIppoOMq9ZzFPLIXxnag_R6GMl0KlMp6q4zftu_5Pt6Tp'
+
+  const notification = {
+    'title': 'State has been changed',
+    'body': 'Light ' + (isOn ? 'on' : 'off'),
+    'icon': 'firebase-logo.png',
+    'click_action': 'https://example02-8bcc0.firebaseapp.com'
+  };
+
+  fetch('https://fcm.googleapis.com/fcm/send', {
+    'method': 'POST',
+    'headers': {
+        'Authorization': 'key=' + key,
+        'Content-Type': 'application/json'
+  },
+    'body': JSON.stringify({
+        'notification': notification,
+        'to': to
+  })
+  }).then(function(response) {
+    console.log(response);
+  }).catch(function(error) {
+    console.error(error);
+  })
 }
 
 Main.prototype.initFirebase = () => {
@@ -62,12 +94,14 @@ Main.prototype.updateState = () => {
   console.log("updateState");
   let elOnOff = document.getElementById('light-onOff');
 
+  const isOn = elOnOff.classList.contains('is-checked');
   let pkg = {
-    OnOff: { on: elOnOff.classList.contains('is-checked') }
+    OnOff: { on: isOn }
   };
 
   console.log(pkg);
   firebase.database().ref('/').child('light').set(pkg);
+  sendMessage(isOn);
 }
 
 window.main = new Main();
